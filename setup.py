b5669206eccaf91eda5cb83b340b441a423e4764
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-08-31 14:31:39 +0800

    alvistack/1.6.0
        git clean -xdf
        tar zcvf ../python-python-discovery_1.6.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-python-discovery.spec ../python-python-discovery_1.6.0-1.spec
        cp ../python*-python-discovery*1.6.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/tox-dev-python-discovery-1.6.0/
        rm -rf ../python*-python-discovery*1.6.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    name='python-discovery',
+    version='1.6.0',
+    description='Python interpreter discovery',
+    maintainer_email='Bernát Gábor <gaborjbernat@gmail.com>',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: 3.15',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+    ],
+    install_requires=[
+        'filelock>=3.15.4',
+    ],
+    packages=[
+        'python_discovery',
+        'python_discovery._windows',
+    ],
+    package_dir={'': 'src'},
+)
